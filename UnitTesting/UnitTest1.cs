3032ff72309e733e300bc242a041c327ab449ddb
zulkiflisulaiman94 <101018091@student.swin.edu.au>
2016-05-27 08:39:30 +1000

    file for zul unit test

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyGame
+{
+    [TestClass]
+    public class TestCase
+    {
+        [TestMethod]
+        public void AIDifficulty()
+        {
+            AIOption _aiopt;
+            _aiopt = AIOption.Hard;
+
+            Assert.IsTrue(_aiopt == AIOption.Hard);
+            
+            _aiopt = AIOption.Medium;
+
+            Assert.IsTrue(_aiopt == AIOption.Medium);
+        }
+
+        [TestMethod]
+        public void CreatingAiPlayer()
+        {
+            AIPlayer _ai1 , _ai2 , _ai3, temp;
+            BattleShipsGame _thegame;
+
+            _thegame = new BattleShipsGame();
+
+            _ai1 = new AIHardPlayer(_thegame);
+            _ai2 = new AIMediumPlayer(_thegame);
+            _ai3 = null;
+            temp = new AIHardPlayer(_thegame);
+
+            Assert.IsTrue(_ai1.GetType() == temp.GetType());
+            Assert.IsFalse(_ai1.GetType() == _ai2.GetType());
+
+            Assert.AreNotEqual(_ai1, null);
+            Assert.AreNotEqual(_ai2, null);
+            Assert.AreEqual(_ai3, null);
+        }
+    }
+}
